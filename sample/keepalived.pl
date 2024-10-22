098b43dcb383d79b543618a1c22dc1dc4ee9e709
runsisi <runsisi@hust.edu.cn>
2024-10-22 09:30:51 +0800

    remove inhert and schema support
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- sample/keepalived.pl
+++ sample/keepalived.pl
@@ -7,9 +7,9 @@ use FindBin;
 use lib "$FindBin::Bin/../perl/lib";
 
 use Config::Neat::Render;
-use Config::Neat::Inheritable;
+use Config::Neat;
 
-my $parser = Config::Neat::Inheritable->new();
+my $parser = Config::Neat->new();
 my $config = $parser->parse_file($ARGV[0]);
 
 my $render = Config::Neat::Render->new({indentation => 2, key_spacing => 1, brace_under => 0, align_all => undef, undefined_value => undef});
